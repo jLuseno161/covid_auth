082285e80774d2c181eaca568be373170968c23b
jLuseno161 <joyluseno2@gmail.com>
2021-08-03 13:02:18 +0300

    deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn covidt.wsgi --log-file -
---
+++ covid/urls.py
@@ -0,0 +1,32 @@
+"""authentication URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+from .views import RegisterAPI
+from knox import views as knox_views
+from .views import LoginAPI
+from django.urls import path
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    
+]
